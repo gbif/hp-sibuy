5b20524b2335a10d8551bb032d76d7f55ec06677
Diego Moreno <damorenov@gmail.com>
2024-10-16 20:26:18 -0500

    Update config.js
    Updating to v2 of config file with specific configuration to filter by country

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -6,11 +6,16 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({
 });
 
 var siteConfig = {
+  version: 2,
+  disableInlineTableFilterButtons: false, // disable option for adding filters by clicking table cells. See https://github.com/gbif/hosted-portals/issues/274
   routes: {
     alwaysUseHrefs: false, // Update - there now is translations. since the site isn't translated we can use push for now. if true, then we will always use hrefs, if false we will use onClick events and push state to the history. I've added this because I just realize that the language picker doesn't work with pushState as the url of the translated site is not updated with the new url
     enabledRoutes: ['occurrenceSearch', 'collectionSearch', 'collectionKey', 'institutionSearch', 'institutionKey', 'datasetKey', 'datasetSearch'],
+    availableCatalogues: ['COLLECTION', 'OCCURRENCE', 'DATASET'],
   },
   occurrence: {
+    highlightedFilters: ['taxonKey', 'stateProvince', 'year', 'basisOfRecord', 'publishingOrg', 'datasetName', 'occurrenceIssue'],
+    excludedFilters: ['occurrenceStatus', 'networkKey', 'hostingOrganizationKey', 'protocol', 'publishingCountryCode', 'institutionCode', 'collectionCode'],
     mapSettings: {
       lat: -34.531472,
       lng: -54.288967,
@@ -31,7 +36,72 @@ var siteConfig = {
         }
       ]
     },
-    highlightedFilters: ['taxonKey', 'stateProvince', 'year', 'basisOfRecord', 'publishingOrg', 'datasetName', 'occurrenceIssue'],
-    occurrenceSearchTabs: ['TABLE', 'MAP', 'GALLERY', 'DATASETS']
-  }
+    occurrenceSearchTabs: ['TABLE', 'MAP', 'GALLERY', 'DATASETS'] // what tabs should be shown
+  },
+  collection: {
+    // filters on the grscicoll institution v1 API https://www.gbif.org/developer/summary
+    // https://hp-theme.gbif-staging.org/data-exploration-config
+    rootFilter: {
+      country: 'UY',
+      displayOnNHCPortal: true,
+      active: true
+    },
+    excludedFilters: ['countrySingleGrSciColl'],// no reason to show a filter on country, when there is only one
+  },
+  institution: {
+    rootFilter: {
+      country: 'UY',
+      displayOnNHCPortal: true,
+      active: true
+    },
+    excludedFilters: ['countrySingleGrSciColl'],// no reason to show a filter on country, when there is only one
+    mapSettings: {
+      enabled: true, // show a map on institution search?
+      lat: -34.531472,
+      lng: -54.288967,
+      zoom: 5
+    },
+  },
+  //literature: {
+    //rootFilter: {
+      //predicate: {
+        //type: 'or', predicates: [
+          //{
+            //type: 'in',
+            //key: 'countriesOfResearcher',
+            //values: ['UY']
+          //},
+          //{
+            //type: 'in',
+            //key: 'countriesOfCoverage',
+            //values: ['UY']
+          //}
+        //]
+      //}
+    //},
+    //highlightedFilters: ['q', 'countriesOfResearcher', 'countriesOfCoverage', 'year']
+  //},
+  dataset: {
+    rootFilter: { publishingCountry: 'UY' },
+    highlightedFilters: ['q', 'anyPublisherKey', 'datasetType', 'license'],
+    excludedFilters: ['publishingCountryCode'],
+  },
+  //apiKeys: {
+    //maptiler: "GET_YOUR_OWN_TOKEN", // https://github.com/gbif/hosted-portals/issues/229
+    //mapbox: "GET_YOUR_OWN__TOKEN"
+  //},
+  maps: {
+    locale: 'es', // we want to show the maps in spanish
+    defaultProjection: 'MERCATOR',
+    defaultMapStyle: 'BRIGHT',
+    mapStyles: {
+      ARCTIC: ['NATURAL', 'BRIGHT'],
+      PLATE_CAREE: ['NATURAL', 'BRIGHT', 'DARK'],
+      MERCATOR: ['NATURAL', 'BRIGHT', 'SATELLITE', 'DARK'],
+      ANTARCTIC: ['NATURAL', 'BRIGHT', 'DARK']
+    }
+  },
+  //messages: { // custom overwrites for the translations, e.g. label the occurrence catalog as a specimen catalog to match our data scope of specimens.
+  //  "catalogues.occurrences": "Specimens"
+  //}
 };
