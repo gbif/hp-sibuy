310c278a1724630337f4ea8f0cb632eb644224fe
Diego Moreno <damorenov@gmail.com>
2024-11-15 08:41:31 -0500

    :wrench: Include config.js
    Include new filter

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -14,7 +14,7 @@ var siteConfig = {
   },
   availableCatalogues: ['COLLECTION', 'OCCURRENCE', 'DATASET', 'INSTITUTION'],
   occurrence: {
-    highlightedFilters: ['taxonKey', 'gadmGid', 'hasCoordinate', 'iucnRedListCategory', 'year', 'basisOfRecord', 'publisherKey'],
+    highlightedFilters: ['taxonKey', 'gadmGid', 'hasCoordinate', 'iucnRedListCategory', 'year', 'basisOfRecord', 'publisherKey', 'q'],
     excludedFilters: ['occurrenceStatus', 'networkKey', 'hostingOrganizationKey', 'protocol', 'publishingCountryCode', 'institutionCode', 'collectionCode'],
     mapSettings: {
       lat: -34.531472,
